4c98978088ec67dff9c64ebf97afecea3ead99bc
Sohail Mahmud <sohailmahmud@yahoo.com>
2021-09-15 15:03:11 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,3 @@
-/*
- * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *  http://aws.amazon.com/apache2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
 
 import 'package:amplify_analytics_pinpoint/amplify_analytics_pinpoint.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
@@ -22,17 +8,6 @@ import 'package:sample_app/Pages/LoadingPage.dart';
 
 import 'Pages/LandingPage.dart';
 
-/*
-IMPORTANT - THIS LINE WILL NOT COMPILE
-That is intentional
-
-You need to generate your own amplifyconfiguration.dart
-for accessing your own AWS resources.
-
-You will use the Amplify CLI tool for that.  Please read the
-README.md contained within the root of this project to learn
-about what you'll need to do.
- */
 import 'amplifyconfiguration.dart';
 
 void main() {
